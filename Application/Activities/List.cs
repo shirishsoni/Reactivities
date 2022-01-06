b0ae3623e043612b2c077c38ec54d8b9d7e6306c
ssoni <ssoni>
2022-01-05 17:05:21 -0800

    End of section 4

---
+++ Application/Activities/List.cs
@@ -0,0 +1,40 @@
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                // this is for how to use cancellation token
+                // try
+                // {
+                //     for (var i = 0; i < 10; i++)
+                //     {
+                //         cancellationToken.ThrowIfCancellationRequested();
+                //         await Task.Delay(1000, cancellationToken);
+                //         _logger.LogInformation($"Task {i} has completed");
+                //     }
+                // }
+                // catch (Exception ex) when (ex is TaskCanceledException)
+                // {
+                //     _logger.LogInformation("Task was cancelled");
+                // }
+                return await _context.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
