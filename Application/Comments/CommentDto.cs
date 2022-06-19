5de27b26c25a1d065fc1b218825a98af3b264111
ssoni <ssoni>
2022-06-18 19:04:55 -0700

    End of section 19 bug fix

--- Application/Comments/CommentDto.cs
+++ Application/Comments/CommentDto.cs
@@ -4,7 +4,7 @@ namespace Application.Comments
     {
         public int Id { get; set; }
         public DateTime CreatedAt { get; set; }
-        public string Boby { get; set; }
+        public string Body { get; set; }
         public string Username { get; set; }
         public string DisplayName { get; set; }
         public string Image { get; set; }
